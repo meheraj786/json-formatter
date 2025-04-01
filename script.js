ec62ed37fcd8dab7adc5537d46e8495f64caafc6
Mehraj Hosen <meherajhosen786@gmail.com>
2025-04-01 15:28:03 +0600

    done

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function submit() {
+      const inputArea = document.getElementById("jsonInput");
+      const outputArea = document.getElementById("jsonOutput");
+      const jsonText = inputArea.value.trim();
+      console.log("click");
+
+      if (jsonText === "") {
+          alert("Please enter a JSON to format!");
+          return;
+      }
+
+      let jsonObj = JSON.parse(jsonText); 
+      let formattedJson = JSON.stringify(jsonObj, null, 4);
+
+      outputArea.value = formattedJson; 
+    }
